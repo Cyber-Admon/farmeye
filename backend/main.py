a91abc9dd8741ddf7d1f81539b2a24ccb769ee4e
Omoniyi Abdulmalik Adebayo (Admon) <pdfrinceadebayo123@gmail.com>
2026-08-19 14:17:44 +0100

    added cors

--- backend/main.py
+++ backend/main.py
@@ -14,6 +14,7 @@ app.add_middleware(
     allow_origins=["*"],
     allow_methods=["*"],
     allow_headers=["*"],
+    expose_headers=["*"],
 )
 
 app.include_router(detection.router, prefix="/api")
